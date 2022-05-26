b318bb9ed3521caacc8c7cc86a288f2d65bd495b
Brian Ketelsen <bketelsen@gmail.com>
2022-05-26 04:55:02 -0400

    update page workaround

--- src/lib/common/navLinks.svelte
+++ src/lib/common/navLinks.svelte
@@ -1,8 +1,7 @@
 <script>
     export let label;
     export let url;
-    $: resolved_url = url === "/" ? "/" : `/pages${url}`;
 </script>
-<a href="{resolved_url}" class="btn btn-ghost btn-sm rounded-btn" > {label} </a>
+<a href="{url}" class="btn btn-ghost btn-sm rounded-btn" > {label} </a>
 
 
